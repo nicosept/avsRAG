f34fa8fc8f4449578a543fed65a854c369badf7f
nicosept <nicoseptiencool@gmail.com>
2025-07-17 23:44:36 -0700

    Refactor RAG class for improved querying and error handling; update WebSocket implementation for abort functionality

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,53 +2,89 @@ import os
 from fastapi import FastAPI, Request, WebSocket, HTTPException, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+import uvicorn
 from dotenv import load_dotenv
-from rag import query_ollama
+from backend.src.rag import RAG
+import asyncio
 import json
 
 load_dotenv()
 PORT = int(os.environ.get("PORT", 5000))
-VITE_PORT = int(os.environ.get("VITE_PORT", 5173))
+FRONT_PORT = int(os.environ.get("FRONT_PORT", 5173))
 
 app = FastAPI()
+
+
 @app.exception_handler(404)
 async def not_found_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=404,
-        content={"detail": "The resource you are looking for was not found."}
+        content={"detail": "The resource you are looking for was not found."},
     )
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[f"http://localhost:{VITE_PORT}"],
+    allow_origins=[f"http://localhost:{FRONT_PORT}"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get('/api/prompt/{prompt}')
-async def prompt(prompt: str):
-    return {"message": query_ollama("gemma3:4b", prompt)}
 
 @app.websocket("/ws/prompt")
 async def websocket_prompt(websocket: WebSocket):
     await websocket.accept()
-    print("WebSocket connection established")
-    try:
-        while True:
-            data = await websocket.receive_text()
-            try:
-              data = json.loads(data)
-            except json.JSONDecodeError:
-              data = [data]
-            for chunk in query_ollama("gemma3:4b", data, stream=True):
-                await websocket.send_text(chunk)
-            await websocket.send_text("__END__")
-    except WebSocketDisconnect:
-        print("WebSocket disconnected")
-    except Exception as e:
-        await websocket.close(code=1000, reason=str(e))
+    async with RAG(stream=True) as rag:
+        print("WebSocket connection established")
+        query_task = None
+        try:
+            while True:
+                data = await websocket.receive_text()
+                try:
+                    data_json = json.loads(data)
+                except json.JSONDecodeError:
+                    data_json = [data]
+
+                if isinstance(data_json, dict) and data_json.get("type") == "abort":
+                    print("Aborting query")
+                    query_task.cancel()
+                    await websocket.send_json(
+                        {"type": "aborted", "message": "Query aborted"}
+                    )
+                    return
+                
+                # If a query task is already running, cancel it
+                # might want to handle this more gracefully with a queue or running multiple queries
+                if query_task and not query_task.done():
+                    query_task.cancel()
+                
+
+                async def query_send_data(data):
+                    try:
+                        async for chunk in rag.query(data):
+                            if chunk is None:
+                                continue
+                            await websocket.send_json(
+                                {"type": "message", "content": chunk}
+                            )
+                    except asyncio.CancelledError:
+                        print("Query task cancelled")
+                        pass
+
+                query_task = asyncio.create_task(query_send_data(data_json))
+
+        except WebSocketDisconnect:
+            print("WebSocket disconnected")
+        except ConnectionError as e:
+            print(f"Connection error: {str(e)}")
+            await websocket.send_json({"type": "error", "message": str(e)})
+            await websocket.close(code=4000, reason=str(e))
+        except Exception as e:
+            print(f"Unknown error: {str(e)}")
+            await websocket.close(code=1000, reason=str(e))
+
+
 if __name__ == "__main__":
-    import uvicorn
-    print(f"Running on port {PORT}, Vite on {VITE_PORT}")
+    print(f"Running backend on port {PORT}, frontend on port {FRONT_PORT}")
     uvicorn.run("main:app", port=PORT, host="127.0.0.1", reload=True, log_level="error")
--- backend/src/rag.py
+++ backend/src/rag.py
@@ -1,30 +1,115 @@
 import ollama
+import httpx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USER_ROLE = os.getenv("USER_ROLE", "user")
+ASSISTANT_ROLE = os.getenv("ASSISTANT_ROLE", "assistant") # Gemma 3 uses "model" as the assistant role
+
+class RAG:
+    """RAG (Retrieval-Augmented Generation) class for querying Ollama models.
+    This class allows you to query a specified Ollama model with a list of messages
+    and an optional context. It can handle both streaming and non-streaming responses.
+    """
+
+    def __init__(self, context: str = None, stream: bool = False):
+        self.model = os.getenv("OLLAMA_MODEL", "gemma3:4b")
+        self.embedding_model = os.getenv("OLLAMA_EMBEDDING_MODEL", "nomic-embed-text")
+        self.llm_client = ollama.AsyncClient(
+            host=os.getenv("OLLAMA_URL", "http://localhost:11434")
+        )
+        self.history = []
+        self.context = context
+        self.stream = stream
+
+    async def close(self):
+        print("Closing RAG instance")
+        await self.llm_client._client.aclose()
+
+    def embed_prompt(self, prompt: str):
+        """Convert a prompt string into a vector."""
+        vector = ollama.embed(model='')
+
+    def set_context(self, context: str):
+        """Set the context for the RAG instance."""
+        self.context = context
+    
+
+    async def query(self, prompt: str):
+        """Query the Ollama model with the provided prompt and context.
+        Args:
+            prompt (list): A list of messages to send to the model.
+        Yields:
+            str: The response from the model, either as a full response or in chunks if streaming is enabled.
+        """
+
+       
+        try:
+            if self.context:
+                prompt = self.context + "\n\n" + prompt
+
+            self.history.append({"role": USER_ROLE, "content": prompt})
+
+            response = await self.llm_client.chat(
+                model=self.model, 
+                messages=self.history, 
+                stream=self.stream
+                )
+            if not response:
+                raise ollama.ResponseError("No response from model.")
+
+            if self.stream:
+                model_reply = ""
+                async for chunk in response:
+                    content = chunk["message"]["content"]
+                    model_reply += content
+                    yield content
+                self.history.append({"role": ASSISTANT_ROLE, "content": model_reply})
+            else:
+                response = await response
+                self.history.append(response)
+                yield response["message"]["content"]
 
 
-def query_ollama(model: str, messages: list, context: str = None, stream: bool = False):
-    if context:
-        system_message = {
-            "role": "system",
-            "content": (
-                "You are a helpful chatbot. Use only the following pieces of context to answer the question. "
-                "Do not hallucinate nor make up new information. You are free to reply that you do not have enough context "
-                "if you do not know the answer to something, not knowing is not a mistake. Only mention the context you have "
-                "if DIRECTLY asked for it: " + context
-            )
-        }
-        messages = [system_message] + messages
-    try:
-        # print(f"Querying Ollama model: {model} with messages: {messages} and stream={stream}")
-        response = ollama.chat(model=model, messages=messages, stream=stream)
+        except ollama.ResponseError as e:
+            raise RuntimeError(f"Error querying Ollama: {e.message}")
+        except httpx.ConnectError as e:
+            raise ConnectionError(f"Connection attempt failed. Please check your Ollama server.")
+        except Exception as e:
+            raise RuntimeError(f"Unexpected error querying Ollama: {str(e)}")
 
-        if not stream:
-            return response["message"]["content"]
-        
-        for chunk in response:
-            yield chunk["message"]["content"]
+    async def __aenter__(self):
+        return self
+    
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.close()
 
-    except ollama.ResponseError as e:
-        if not stream:
-            return f"Error querying Ollama: {e.message}"
-        else:
-            yield f"Error querying Ollama: {e.message}"
+
+# def query_ollama(model: str, messages: list, context: str = None, stream: bool = False):
+#     if context:
+#         messages = [ messages, {
+#             "role": "user",
+#             "content": (
+#                 "You are a helpful chatbot. Use only the following pieces of context to answer the question. "
+#                 "Do not hallucinate nor make up new information. You are free to reply that you do not have enough context "
+#                 "if you do not know the answer to something, not knowing is not a mistake. Only mention the context you have "
+#                 "if DIRECTLY asked for it: " + context + "\n\n" + messages[-1]["content"]
+#             )
+#         }]
+#     try:
+#         # print(f"Querying Ollama model: {model} with messages: {messages} and stream={stream}")
+#         response = ollama.chat(model=model, messages=messages, stream=stream)
+
+#         if not stream:
+#             return response["message"]["content"]
+
+#         for chunk in response:
+#             yield chunk["message"]["content"]
+
+#     except ollama.ResponseError as e:
+#         if not stream:
+#             return f"Error querying Ollama: {e.message}"
+#         else:
+#             yield f"Error querying Ollama: {e.message}"
